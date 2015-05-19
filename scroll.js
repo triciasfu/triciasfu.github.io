d7868f94688a8da53cce40bc7c6aa723e9c3d4c2
Tricia Fu <triciasfu@gmail.com>
2015-05-19 16:46:09 -0700

    added smooth scroll

--- scroll.js
+++ scroll.js
@@ -1,46 +1,26 @@
-//  $(function() {
-//     $('a[href*=#]:not([href=#])').click(function() {
-//         if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') || location.hostname == this.hostname) {
+$(document).ready(function() {
+  function filterPath(string) {
+    return string
+    .replace(/^\//,'')
+    .replace(/(index|default).[a-zA-Z]{3,4}$/,'')
+    .replace(/\/$/,'');
+  }
+  $('a[href*=#]').each(function() {
+    if ( filterPath(location.pathname) == filterPath(this.pathname)
+      && location.hostname == this.hostname
+      && this.hash.replace(/#/,'') ) {
+      var $targetId = $(this.hash), $targetAnchor = $('[name=' + this.hash.slice(1) +']');
+    var $target = $targetId.length ? $targetId : $targetAnchor.length ? $targetAnchor : false;
+    if ($target) {
+     var targetOffset = $target.offset().top;
+     $(this).click(function() {
+       $('html, body').animate({scrollTop: targetOffset}, 1500);
 
-//             var target = $(this.hash);
-//             target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-//             if (target.length) {
-//                 $('html,body').animate({
-//                     scrollTop: target.offset().top
-//                 }, 1000);
-//                 return false;
-//             }
-//         }
-//     });
-// });
+       window.scrollTo(0,scrollToM);
 
-
-// (function (jQuery) {
-//   jQuery.mark = {
-//     jump: function (options) {
-//       var defaults = {
-//         selector: 'a.scroll-on-page-link'
-//       };
-//       if (typeof options == 'string') {
-//         defaults.selector = options;
-//       }
-
-//       options = jQuery.extend(defaults, options);
-//       return jQuery(options.selector).click(function (e) {
-//         var jumpobj = jQuery(this);
-//         var target = jumpobj.attr('href');
-//         var thespeed = 1000;
-//         var offset = jQuery(target).offset().top;
-//         jQuery('html,body').animate({
-//           scrollTop: offset
-//         }, thespeed, 'swing');
-//         e.preventDefault();
-//       });
-//     }
-//   };
-// })(jQuery);
-
-
-// jQuery(function(){
-//   jQuery.mark.jump();
-// });
+       return false;
+     });
+   }
+ }
+});
+});
